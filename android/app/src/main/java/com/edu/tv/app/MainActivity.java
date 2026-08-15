fc6b4f227e05960ba1e72c20a7eb584f1bb39aed
ienpan <ienpan@163.com>
2026-08-15 14:33:45 +0800

    fix: 移除已废弃的Keyboard插件注册

--- android/app/src/main/java/com/edu/tv/app/MainActivity.java
+++ android/app/src/main/java/com/edu/tv/app/MainActivity.java
@@ -4,9 +4,8 @@ import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        registerPlugin(com.getcapacitor.plugin.Keyboard.class);
-        super.onCreate(savedInstanceState);
-    }
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
 }
